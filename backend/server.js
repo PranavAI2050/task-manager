8e656eb12b4cb9eede5833c836880a814b870026
PranavAI2050 <atotepranavdipak@gmail.com>
2024-11-17 09:45:51 +0545

    changed the cors again

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({
-  origin: 'https://task-manager-backend-ad77.onrender.com',
+  origin: 'https://task-manager-frontend-qpco.onrender.com',
   optionsSuccessStatus: 200
 }));
 
