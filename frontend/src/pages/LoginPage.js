ac7082c43c644a869b85806347d413c0824f6c74
Pranav Atote <123180829+PranavAI2050@users.noreply.github.com>
2025-05-19 14:20:23 +0545

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { useNavigate , Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { loginUser } from "../services/api";
 import { setAuthToken } from "../services/api";
+import './LoginPage.css';  // Import the CSS file
 
 const LoginPage = ({ setToken }) => {
   const [email, setEmail] = useState("");
@@ -22,13 +23,7 @@ const LoginPage = ({ setToken }) => {
   };
 
   return (
-    
-    <div>
-        <h1>Login</h1>
-        <p>
-          Don't have an account?{" "}
-          <Link to="/register">Register</Link> here.
-        </p>
+    <div className="container">
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
         <input
@@ -45,6 +40,10 @@ const LoginPage = ({ setToken }) => {
         />
         <button type="submit">Login</button>
       </form>
+      <p>
+        Don't have an account?{" "}
+        <Link to="/register">Register</Link> here.
+      </p>
     </div>
   );
 };
